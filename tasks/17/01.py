bc03348a9324b735fc226206f02172feb7f0f1d5
Андрей Беляков <ttxiom@gmail.com>
2022-08-26 10:44:20 +0500

    task17

---
+++ tasks/17/01.py
@@ -0,0 +1,16 @@
+f = open("17.txt")
+
+lst = [abs(int(line)) for line in f]
+
+max3 = max([x for x in lst if x%3==0])
+
+pairs = []
+max_sum = -100000
+for i in range(len(lst)-1):
+    a, b = lst[i], lst[i+1]
+    if int(a%10==3) + int(b%10==3) == 1:
+        if a**2 + b**2 >= max3**2:
+            pairs.append((a,b))
+            max_sum = max(max_sum, a**2 + b**2)
+
+print(len(pairs), max_sum)
---
+++ tasks/17/02.py
@@ -0,0 +1,16 @@
+lst, max3 = [], 0
+for line in open("17.txt"):
+    elm = abs(int(line))
+    lst.append(elm)
+    if elm%3==0:
+        max3 = max(max3, elm)
+
+cnt, max_sum = 0, 0
+for i in range(len(lst)-1):
+    a, b = lst[i], lst[i+1]
+    if (a%10==3) ^ (b%10==3):
+        if a**2 + b**2 >= max3**2:
+            cnt += 1
+            max_sum = max(max_sum, a**2 + b**2)
+
+print(cnt, max_sum)
