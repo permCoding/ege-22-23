1202d381962e0380183aa7cbb008b27ac98e71b4
Андрей Беляков <ttxiom@gmail.com>
2022-09-10 09:10:26 +0500

    Create 01.py

---
+++ tasks/06/01.py
@@ -0,0 +1,23 @@
+from turtle import *
+
+color('red', 'yellow')
+shape('turtle')
+speed(0)
+left(90)
+x0, y0 = -200, -200
+penup()
+goto(x0, y0)
+pendown()
+step = 50
+
+begin_fill()
+for _ in range(7):
+    forward(10*step)
+    right(120)
+end_fill()
+
+penup()
+for x in range(12):
+    for y in range(12):
+        setpos(x0+x*step, y0+y*step)
+        dot(3, 'black')
