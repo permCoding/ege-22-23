3466935c861f0e20d76db48537896d5c71fe526a
Андрей Беляков <ttxiom@gmail.com>
2022-09-10 15:01:53 +0500

    Create 04.py

---
+++ days/2022-09-10/04.py
@@ -0,0 +1,25 @@
+from turtle import *
+
+step = 40
+color("red")
+width(2)
+speed(4)
+
+for i in range(6):
+    forward(10 * step)
+    left(90)
+    forward(1 * step)
+    left(90)
+    forward(10 * step)
+    right(90)
+    forward(1 * step)
+    right(90)
+
+##forward(10 * step)
+##left(90)
+##forward(1 * step)
+##left(90)
+##forward(10 * step)
+##right(90)
+##forward(1 * step)
+##right(90)
