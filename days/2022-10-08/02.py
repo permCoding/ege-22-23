ffbee7eb730d32ca0ece4d5167e11b809bf8018e
Андрей Беляков <ttxiom@gmail.com>
2022-10-08 11:31:40 +0500

    up

---
+++ days/2022-10-08/02.py
@@ -0,0 +1,25 @@
+import os
+
+os.system('cls')
+
+n = 4
+b = bin(n)
+print(b)
+print(type(b))
+
+s = '179235812'
+print(s[0:3])
+print(s[:3])
+print(s[3:])
+print(s[:])
+
+print(s[-1])
+print(s[-2])
+print(s[-3])
+
+print(s[0])
+print(s[1])
+print(s[2])
+
+print(s[::2])
+print(s[::-1])
---
+++ days/2022-10-08/03.py
@@ -0,0 +1,28 @@
+import os
+
+os.system('cls')
+
+# ver 1
+for n in range(1, 100):
+    b = bin(n)[2:]  # 1. Строится двоичная запись числа N
+    cnt = b.count('1')
+    if cnt % 2 == 0:
+        b += '0'
+        b = '10' + b[2:]
+    else:
+        b += '1'
+        b = '11' + b[2:]
+    r = int(b, 2)
+    if r > 40:
+        print(n)
+        break 
+
+# ver 2
+# n = 1
+# while True:
+#     pass
+#     pass
+#     if r > 40:
+#         print(n)
+#         break
+#     n += 1
