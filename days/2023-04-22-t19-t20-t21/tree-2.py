258657baceb9a0a0188bb478d6517e949e969885
permCoding <ttxiom@gmail.com>
2023-04-19 14:38:29 +0500

    tree-3

--- days/2023-04-22-t19-t20-t21/tree-2.py
+++ days/2023-04-22-t19-t20-t21/tree-2.py
@@ -1,29 +1,32 @@
 # dct = { 1: [4, 7] }
+# тут узлы могут иметь совпадающие значения
 tree = {
-    1: [4, 3],
-    4: [9, 7],
-    9: [None, None],
-    7: [2, None],
-    2: [None, None],
-    3: [6, 8],
-    6: [None, None],
-    8: [None, None]
+    0: [1, 1, 2],
+    1: [4, 3, 4],
+    2: [3, 5, 6],
+    3: [3, None, None],
+    4: [7, 9, None],
+    5: [4, None, None],
+    6: [7, None, None],
+    9: [1, None, None]
 }
+
+# print(tree.keys())  # все узлы
 
 def get_max_path(root):
     if root == None: return 0
-    left_path_max = get_max_path(tree[root][0])
-    right_path_max = get_max_path(tree[root][1])
-    return root + max(left_path_max, right_path_max)
+    left_path_max = get_max_path(tree[root][1])
+    right_path_max = get_max_path(tree[root][2])
+    return tree[root][0] + max(left_path_max, right_path_max)
 
 
-root = 1
+root = 0
 print(get_max_path(root))
 
 '''
           1
     4          3
- 9     7    6     8 
-N N   2 N  N N   N N
+ 3     7    4     7 
+N N   1 N  N N   N N
 
 '''
