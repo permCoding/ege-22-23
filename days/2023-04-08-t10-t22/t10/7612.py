4d03e9ab49ca88ed68a3232470697a81c5823a3d
Андрей Беляков <ttxiom@gmail.com>
2023-04-20 18:41:29 +0500

    up

--- days/2023-04-08-t10-t22/t10/7612.py
+++ days/2023-04-08-t10-t22/t10/7612.py
@@ -1,16 +1,25 @@
 import re
 
-txt = open('demo-2023-10.txt').read()
+#txt = open('10_7612.txt', encoding='utf8').read()
+txt = open('10_7612.txt').read()
 
-lst = re.findall('[^а-я][вВ]ечер[^а-я]', txt)
-print(len(lst))
-print(lst) # [' вечер ', ' вечер;']
+#lst = re.findall('[^а-я][вВ]ечер[^а-я]', txt)
+#print(len(lst)) # это отдельные слова
+#print(lst) # [' вечер ', ' вечер;']
 
-lst = re.findall('[^а-я]вечер[а-я]+', txt)
-print(len(lst))
-for elm in lst: print(elm)
+lst = re.findall('[^а-я]вечер[а-я]+', txt, re.I)
+print(len(lst)) # слова которые начинаются на вечер...
+print(lst)
 
-'''
+lst = re.findall('[а-я]+вечер[^а-я]', txt, re.I)
+print(len(lst)) # слова которые заканчиваются на ...вечер
+print(lst)
+
+lst = re.findall('[а-я]+вечер[а-я]+', txt, re.I)
+print(len(lst)) # слова в которых внутри ...вечер...
+print(lst)
+
+'''V
 [а-я][Вв]ечер[^а-я] = 0 - заканчиваются на вечер
 [^а-я][Вв]ечер[а-я] = 40 - начинаются на вечер
 [а-я]вечер[а-я] = __ - 'вечер' внутри слова
